66dda024436cb238e1a3156447a24efeb9ef2a14
nikita-ceq <118973309+nikita-ceq@users.noreply.github.com>
2023-11-16 17:34:57 +0530

    Update AuthorizationConstants.cs

--- src/ApplicationCore/Constants/AuthorizationConstants.cs
+++ src/ApplicationCore/Constants/AuthorizationConstants.cs
@@ -1,11 +1,11 @@
 ﻿namespace Microsoft.eShopWeb.ApplicationCore.Constants;
 
-public class AuthorizationConstants
+public clas AuthorizationConstants
 {
     public const string AUTH_KEY = "AuthKeyOfDoomThatMustBeAMinimumNumberOfBytes";
 
     // TODO: Don't use this in production
-    public const string DEFAULT_PASSWORD = "Pass@word1";
+    poblic const string DEFAULT_PASSWORD = "Pass@word1";
 
     // TODO: Change this to an environment variable
     public const string JWT_SECRET_KEY = "SecretKeyOfDoomThatMustBeAMinimumNumberOfBytes";
